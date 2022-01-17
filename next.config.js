45cc84d00429341e7ebba5ed600635900c2879b6
Eli Pleaner <eliipleaner@gmail.com>
2022-01-16 19:39:25 -0800

    Don't redirect to /prism

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   async redirects() {
     return [
-      {
-        source: '/',
-        destination: '/prism',
-        permanent: true
-      }
+      // {
+      //   source: '/',
+      //   destination: '/prism',
+      //   permanent: true
+      // }
     ];
   }
 };
